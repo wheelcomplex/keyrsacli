48beaecadac724b5442b632dbdfb50b42f72b973
YinJiaJin <yinjiajin@xiaomi.com>
2015-07-04 16:45:41 +0800

    multi-routine keyrsa crypt work

--- keyrsacli.go
+++ keyrsacli.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/wheelcomplex/preinit/getopt"
+	"github.com/wheelcomplex/preinit/keyaes"
 	"github.com/wheelcomplex/preinit/keyrsa"
 	"github.com/wheelcomplex/preinit/misc"
 )
@@ -21,11 +22,12 @@ func main() {
 	time.Sleep(1e8)
 
 	key := opt.OptVarString("-k/--key", "2.71828182845904523536028747135266249775724709369995957496696762", "input key string")
-	rsalen := opt.OptVarInt("-l/--lenght", "2k", "rsa key lenght")
+	rsalen := opt.OptVarInt("-r/--lenght", "2k", "rsa key lenght")
 	size := opt.OptVarInt("-s/--size", "256", "size of plain text")
 	cpus := opt.OptVarInt("-c/--cpu", "-1", "running cpus")
 	//workers := opt.OptVarInt("-w/--worker", "0", "number of aes worker")
-	loop := opt.OptVarInt("-l/--loop", "1m", "number of stress loop")
+	loop := opt.OptVarInt("-l/--loop", "0", "number of stress loop")
+	looptype := opt.OptVarInt("-t/--type", "0", "type of stress loop, 0 for all, 1 for encrypt, 2 for decrypt")
 	//vb := opt.OptVarBool("-v/--verb", "false", "debug")
 
 	if opt.OptVarBool("-h/--help", "false", "show help") {
@@ -36,13 +38,14 @@ func main() {
 		opt.Usage()
 		os.Exit(1)
 	}
+	cpus = misc.SetGoMaxCPUs(cpus)
 	starts := time.Now()
 	prets := starts
 
-	enRc := keyrsa.NewKeyRSACrypto([]byte(key), rsalen)
-	deRc := keyrsa.NewKeyRSACrypto([]byte(key), rsalen)
+	enRc := keyrsa.NewKeyRSACrypto([]byte(key), rsalen, cpus)
+	deRc := keyrsa.NewKeyRSACrypto([]byte(key), rsalen, cpus)
 
-	misc.Tpf("%d cpus, payload size %d, stress loop %d\n", misc.SetGoMaxCPUs(cpus), size, loop)
+	misc.Tpf("%d cpus, payload size %d, stress loop %d\n", cpus, size, loop)
 	plaintext := enRc.GetBuf(size)
 	//	for i := 0; i < 1000; i++ {
 	misc.MemSetBytes(plaintext, 255)
@@ -53,7 +56,7 @@ func main() {
 	ohash := enRc.Hash(plaintext)
 	osign, oerr := enRc.SignOrigPKCS1v15(plaintext)
 	if oerr != nil {
-		tpf("%s\n", oerr.Error())
+		tpf("SignOrigPKCS1v15: %s\n", oerr.Error())
 		os.Exit(1)
 	}
 	overify := enRc.VerifyOrigPKCS1v15(plaintext, osign)
@@ -81,9 +84,10 @@ func main() {
 	prets = time.Now()
 	ciphertext, err := enRc.NonceEncrypt(plaintext)
 	if err != nil {
-		tpf("%s\n", err.Error())
+		tpf("NonceEncrypt: %s\n", err.Error())
 		os.Exit(1)
 	}
+	rsaenEsp := time.Now().Sub(prets)
 	misc.Tpf("Encrypt, %v\n", time.Now().Sub(prets))
 	prets = time.Now()
 	displen = len(ciphertext)
@@ -94,13 +98,14 @@ func main() {
 	prets = time.Now()
 	msg, err := deRc.NonceDecrypt(ciphertext)
 	if err != nil {
-		tpf("%s\n", err.Error())
+		tpf("NonceDecrypt: %s\n", err.Error())
 		os.Exit(1)
 	}
+	rsadeEsp := time.Now().Sub(prets)
 	misc.Tpf("Decrypt, %v\n", time.Now().Sub(prets))
 	prets = time.Now()
-	dsum := deRc.Sum(plaintext)
-	dhash := deRc.Hash(plaintext)
+	dsum := deRc.Sum(msg)
+	dhash := deRc.Hash(msg)
 	displen = len(msg)
 	if displen > 16 {
 		displen = 16
@@ -111,47 +116,83 @@ func main() {
 	if ohash != dhash {
 		tpf("Encrypt/Decrypt hash mismatch\n")
 	}
-	//	//
-	//	for i := 0; i < 10; i++ {
-	//		ciphertext, err = enRc.NonceEncrypt(plaintext)
-	//		if err != nil {
-	//			tpf("%s\n", err.Error())
-	//			os.Exit(1)
-	//		}
-	//		enRc.PutBuf(ciphertext)
-	//	}
-	//	misc.Tpf("NonceEncrypt X 10, %v\n", time.Now().Sub(prets))
-	//	ciphertext, err = enRc.NonceEncrypt(plaintext)
-	//	prets = time.Now()
-	//	for i := 0; i < 10; i++ {
-	//		msg, err = deRc.NonceDecrypt(ciphertext)
-	//		if err != nil {
-	//			tpf("%s\n", err.Error())
-	//			os.Exit(1)
-	//		}
-	//		deRc.PutBuf(msg)
-	//	}
-	//	misc.Tpf("NonceDecrypt X 10, %v\n", time.Now().Sub(prets))
-	//	prets = time.Now()
+	deRc.PutBuf(msg)
 
-	misc.Tpf("stress test, %d loops\n", loop)
+	// try aes
+
+	aesEn := keyaes.NewEncryptAES([]byte(key), nil, cpus)
+	aesDe := keyaes.NewDecryptAES([]byte(key), nil, cpus)
 	prets = time.Now()
-	loopts := prets
-	for i := 0; i < loop; i++ {
-		ciphertext, err = enRc.NonceEncrypt(plaintext)
-		if err != nil {
-			tpf("%s\n", err.Error())
-			os.Exit(1)
-		}
-		msg, err = deRc.NonceDecrypt(ciphertext)
-		if err != nil {
-			tpf("%s\n", err.Error())
-			os.Exit(1)
+	aesframe := aesEn.Encrypt(nil, plaintext)
+	displen = len(aesframe)
+	if displen > 16 {
+		displen = 16
+	}
+	aesenEsp := time.Now().Sub(prets)
+	misc.Tpf("AesEncrypt, %v\n", time.Now().Sub(prets))
+	tpf("\t AesEncrypt(%d): %v\n", len(aesframe), aesframe[:displen])
+	prets = time.Now()
+	aesplaintext, err := aesDe.Decrypt(aesframe)
+	if err != nil {
+		tpf("AES Decrypt: %s\n", err.Error())
+		os.Exit(1)
+	}
+	aesdeEsp := time.Now().Sub(prets)
+	displen = len(aesframe)
+	if displen > 16 {
+		displen = 16
+	}
+	misc.Tpf("AesDecrypt, %v\n", time.Now().Sub(prets))
+	tpf("\t AesDecrypt(%d): %v\n", len(aesplaintext), aesplaintext[:displen])
+	prets = time.Now()
+	aesEnhash := keyaes.Murmur3Sum32(plaintext)
+	aesDehash := keyaes.Murmur3Sum32(aesplaintext)
+	tpf("\t aesEnhash: %d\n", aesEnhash)
+	tpf("\t aesEnhash: %d\n", aesEnhash)
+	if aesEnhash != aesDehash {
+		tpf("AES Encrypt/Decrypt hash mismatch\n")
+	}
+	tpf("\t Encrypt RSA/AES: %d / %d = %d(%s)\n", rsaenEsp, aesenEsp, rsaenEsp/aesenEsp, misc.ItoKMG(int(rsaenEsp/aesenEsp)))
+	tpf("\t Decrypt RSA/AES: %d / %d = %d(%s)\n", rsadeEsp, aesdeEsp, rsadeEsp/aesdeEsp, misc.ItoKMG(int(rsadeEsp/aesdeEsp)))
+	tpf("\t RSA Decrypt/Encrypt: %d / %d = %d(%s)\n", rsadeEsp, rsaenEsp, rsadeEsp/rsaenEsp, misc.ItoKMG(int(rsadeEsp/rsaenEsp)))
+	aesEn.PutBuf(aesframe)
+	aesDe.PutBuf(aesplaintext)
+
+	if loop > 0 {
+		prets = time.Now()
+		loopts := prets
+		var esp time.Duration
+		var qps uint64
+		if looptype == 2 || looptype == 0 {
+			misc.Tpf("Decrypt stress test, %d loops\n", loop)
+			for i := 0; i < loop; i++ {
+				msg, err = deRc.NonceDecrypt(ciphertext)
+				if err != nil {
+					tpf("NonceDecrypt: %s\n", err.Error())
+					os.Exit(1)
+				}
+				deRc.PutBuf(msg)
+			}
+			esp = time.Now().Sub(loopts)
+			qps = uint64(loop) * uint64(time.Second) / uint64(esp)
+			misc.Tpf("Decrypt stress: qps %d(%s), esp %v\n", qps, misc.ItoKMG(int(qps)), esp)
 		}
 		enRc.PutBuf(ciphertext)
-		deRc.PutBuf(msg)
+		prets = time.Now()
+		loopts = prets
+		if looptype == 1 || looptype == 0 {
+			misc.Tpf("Encrypt stress test, %d loops\n", loop)
+			for i := 0; i < loop; i++ {
+				ciphertext, err = enRc.NonceEncrypt(plaintext)
+				if err != nil {
+					tpf("NonceEncrypt: %s\n", err.Error())
+					os.Exit(1)
+				}
+				enRc.PutBuf(ciphertext)
+			}
+			esp = time.Now().Sub(loopts)
+			qps = uint64(loop) * uint64(time.Second) / uint64(esp)
+			misc.Tpf("Encrypt stress: qps %d(%s), esp %v\n", qps, misc.ItoKMG(int(qps)), esp)
+		}
 	}
-	esp := time.Now().Sub(loopts)
-	qps := uint64(loop) * uint64(time.Second) / uint64(esp)
-	misc.Tpf("stress: qps %d(%s), esp %v\n", qps, misc.ItoKMG(int(qps)), esp)
 }
